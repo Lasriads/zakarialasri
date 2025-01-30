2c7bc47ac0974eb815e1259c705def96cfbaf929
Zakaria lasri <ziko06man@gmail.com>
2025-01-30 21:03:46 +0100

    update js code

--- js/custom.js
+++ js/custom.js
@@ -35,3 +35,13 @@
 
 })(jQuery);
 
+function SendMail() {
+  var params = { 
+    from_name: document.getElementById("fullName").value,
+    email_id : document.getElementById("email_id").value,
+    message : document.getElementById("message").value
+  }
+  emailjs.send("service_mv2a5np", "template_athvw5f", params).then(function (res) {
+    alert("Succes! " + res.status);
+  })
+}
